a3f482eaf84f8fe042ccb4e983439005fbacd0aa
fpemba <fatuma.pemba@yahoo.com>
2020-10-04 09:58:54 -0400

    added getPreviousTransaction method

--- src/org/wecancodeit/BankAccount.java
+++ src/org/wecancodeit/BankAccount.java
@@ -14,11 +14,25 @@ public class BankAccount {
         }
     }
 
-    void withdraw(int amount){
-        if(amount != 0){
-            balance-= amount;
+    void withdraw(int amount) {
+        if (amount != 0) {
+            balance -= amount;
             previousTransaction = -amount;
         }
+    }
+
+    void getPreviousTransaction() {
+        if (previousTransaction > 0) {
+            System.out.println("Deposited: " + previousTransaction);
+        } else if (previousTransaction < 0) {
+            System.out.println("Withdrawn: " + Math.abs(previousTransaction));
+        } else {
+            System.out.println("No transaction occured");
+        }
+    }
+
+    void showMenu() {
+
     }
 
 }
