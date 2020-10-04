b13bf7d407b4255e1cd308fd4c432a5f4fe48342
fpemba <fatuma.pemba@yahoo.com>
2020-10-04 10:25:40 -0400

    finished do-while look and switch statement

--- src/org/wecancodeit/BankAccount.java
+++ src/org/wecancodeit/BankAccount.java
@@ -1,5 +1,7 @@
 package org.wecancodeit;
 
+import java.util.Scanner;
+
 public class BankAccount {
 
     private int balance;
@@ -39,7 +41,70 @@ public class BankAccount {
 
     void showMenu() {
 
+        char option = '\0';
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Welcome " + customerName);
+        System.out.println("Your ID is: " + customerId);
+        System.out.println("\n");
+        System.out.println("A. Check Balance");
+        System.out.println("B. Deposit");
+        System.out.println("C. Withdraw");
+        System.out.println("D. Previous transaction");
+        System.out.println("E. Exit");
+
+        do{
+            System.out.println("============================================================");
+            System.out.println("Enter an option");
+            System.out.println("============================================================");
+            option = input.next().charAt(0);
+            System.out.println("\n");
+
+            switch (option){
+                case 'A':
+                    System.out.println("-----------------------------------");
+                    System.out.println("Balance = " + balance);
+                    System.out.println("-----------------------------------");
+                    System.out.println("\n");
+                    break;
+
+                case 'B':
+                    System.out.println("-----------------------------------");
+                    System.out.println("Enter an amount to deposit");
+                    System.out.println("-----------------------------------");
+                    int amount = input.nextInt();
+                    deposit(amount);
+                    System.out.println("Your balance is now: " + balance);
+                    System.out.println("\n");
+                    break;
+
+                case 'C':
+                    System.out.println("-----------------------------------");
+                    System.out.println("Enter an amount to withdraw");
+                    System.out.println("-----------------------------------");
+                    int amount2 = input.nextInt();
+                    withdraw(amount2);
+                    System.out.println("Your balance is now: " + balance);
+                    System.out.println("\n");
+                    break;
+
+                case 'D':
+                    System.out.println("-----------------------------------");
+                    getPreviousTransaction();
+                    System.out.println("-----------------------------------");
+                    System.out.println("\n");
+                    break;
+
+                case 'E':
+                    System.out.println("***********************************");
+                    System.out.println("Thank you for using our ATM!");
+                    break;
+
+                default:
+                    System.out.println("Invalid Option! Please enter again.");
+            }
+
+        } while(option != 'E');
     }
 
 }
--- src/org/wecancodeit/BankingApplication.java
+++ src/org/wecancodeit/BankingApplication.java
@@ -3,6 +3,8 @@ package org.wecancodeit;
 public class BankingApplication {
 
     public static void main(String[] args) {
-	// write your code here
+	BankAccount ac1 = new BankAccount("Fatuma Pemba","12345");
+
+	ac1.showMenu();
     }
 }
